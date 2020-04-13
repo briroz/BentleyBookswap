bce2f3ea9a56a777ef432465bd27824854e81d08
Brian <rosmus_bria@bentley.edu>
2020-04-13 14:43:45 -0400

    Adding New Sorting Activity

---
+++ app/src/main/java/com/briroz/bentleybookswap/SortBuyingListActivity.java
@@ -0,0 +1,14 @@
+package com.briroz.bentleybookswap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SortBuyingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort_buying_list);
+    }
+}
