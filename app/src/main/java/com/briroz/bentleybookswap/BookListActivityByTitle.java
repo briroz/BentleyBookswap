97f6bb7e9ee758ab6e3987e0d009b9e802f39623
Brian <rosmus_bria@bentley.edu>
2020-04-13 15:01:16 -0400

    Added ListView Activities for Book viewing

--- app/src/main/java/com/briroz/bentleybookswap/BookListActivityByTitle.java
+++ app/src/main/java/com/briroz/bentleybookswap/BookListActivityByTitle.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SortBuyingListActivity extends AppCompatActivity {
+public class BookListActivityByTitle extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sort_buying_list_activity);
+        setContentView(R.layout.activity_book_list_by_title);
     }
 }
