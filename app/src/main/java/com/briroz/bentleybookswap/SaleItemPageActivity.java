df426b9570c2dd1bf1e4bfd35139065c03307095
Brian <rosmus_bria@bentley.edu>
2020-04-13 18:40:44 -0400

    Added Item Page Activity

---
+++ app/src/main/java/com/briroz/bentleybookswap/SaleItemPageActivity.java
@@ -0,0 +1,14 @@
+package com.briroz.bentleybookswap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaleItemPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sale_item_page);
+    }
+}
