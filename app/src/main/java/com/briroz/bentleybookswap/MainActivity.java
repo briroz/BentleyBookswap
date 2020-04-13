5423f27d3d7cb479887265c2ced336a9b4ac4305
Brian <rosmus_bria@bentley.edu>
2020-04-13 13:45:01 -0400

    New Activities, linking them with buttons

--- app/src/main/java/com/briroz/bentleybookswap/MainActivity.java
+++ app/src/main/java/com/briroz/bentleybookswap/MainActivity.java
@@ -2,15 +2,27 @@ package com.briroz.bentleybookswap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonSell1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSell1 = findViewById(R.id.buttonSell);
+        buttonSell1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(getApplicationContext(),SellBookActivity.class);
+        startActivity(i);
+        setContentView(R.layout.add_item_activity);
     }
 }
---
+++ app/src/main/java/com/briroz/bentleybookswap/SellBookActivity.java
@@ -0,0 +1,15 @@
+package com.briroz.bentleybookswap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item_activity);
+
+    }
+}
