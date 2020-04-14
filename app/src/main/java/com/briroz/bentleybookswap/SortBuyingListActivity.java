78226e924f11cb3e42712bc1b2c40fbba30ceb7c
Brian <rosmus_bria@bentley.edu>
2020-04-13 21:30:26 -0400

    Fixed Buttons on Main Menu

--- app/src/main/java/com/briroz/bentleybookswap/SortBuyingListActivity.java
+++ app/src/main/java/com/briroz/bentleybookswap/SortBuyingListActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SortBuyingListActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonClassSort1;
+    private Button buttonTitleSort1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sort_buying_list_activity);
+        buttonClassSort1 = findViewById(R.id.buttonClassSort);
+        buttonClassSort1.setOnClickListener(this);
+        buttonTitleSort1 = findViewById(R.id.buttonTitleSort);
+        buttonTitleSort1.setOnClickListener(this);
+
     }
 
     @Override
